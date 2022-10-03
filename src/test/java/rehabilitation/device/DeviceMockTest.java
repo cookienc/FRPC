3bf42724beaca909ab0facfb0268bbcba7243699
kookiedncream <kim76744@naver.com>
2022-10-03 16:15:18 +0900

    test: 같은날 여러 번 기록을 하면 평균을 내서 나오도록 하는 기능 테스트 추가

--- src/test/java/rehabilitation/device/DeviceMockTest.java
+++ src/test/java/rehabilitation/device/DeviceMockTest.java
@@ -1,6 +1,5 @@
 package rehabilitation.device;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,9 +18,12 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("꺽은선 그래프 기능을 테스트 한다")
 public class DeviceMockTest {
 
 	@InjectMocks
@@ -54,6 +56,47 @@ public class DeviceMockTest {
 		List<PressureResponse> 응답 = deviceService.getLineGraph(now);
 
 		//then
-		Assertions.assertThat(응답).hasSize(5);
+		assertAll(
+				() -> assertThat(응답).hasSize(5),
+				() -> assertThat(응답.get(0).getThumb()).isEqualTo(25)
+		);
+	}
+
+	/**
+	 * given 날짜가 같은 그래프 값을 5개 이상 추가한다.
+	 * when 꺽은선 그래프 정보를 조회하면
+	 * then 1개만 반환된다.
+	 */
+	@DisplayName("날짜가 같은 5개의 그래프를 추가하여 조회하면 1개만 출력된다")
+	@Test
+	void 날짜가_같은_5개의_그래프를_추가하여_조회하면_1개만_출력된다() {
+		//given
+		LocalDateTime now = LocalDateTime.now();
+		LocalDate date = now.toLocalDate();
+		LocalDateTime start = LocalDateTime.of(date, LocalTime.of(0, 0, 0));
+		LocalDateTime end = LocalDateTime.of(date, LocalTime.of(23, 59, 59));
+
+		Pressure p1 = new Pressure(new Finger(25, 25, 25, 25, 25));
+		ReflectionTestUtils.setField(p1, "createdAt", start);
+		Pressure p2 = new Pressure(new Finger(26, 26, 26, 26, 26));
+		ReflectionTestUtils.setField(p2, "createdAt", start);
+		Pressure p3 = new Pressure(new Finger(27, 27, 27, 27, 27));
+		ReflectionTestUtils.setField(p3, "createdAt", start);
+		Pressure p4 = new Pressure(new Finger(30, 30, 30, 30, 30));
+		ReflectionTestUtils.setField(p4, "createdAt", start);
+		Pressure p5 = new Pressure(new Finger(35, 35, 35, 35, 35));
+		ReflectionTestUtils.setField(p5, "createdAt", start);
+
+		when(pressureRepository.findByCreatedAtBetween(start, end)).thenReturn(List.of(p1, p2, p3, p4, p5));
+
+		//when
+		List<PressureResponse> 응답 = deviceService.getLineGraph(now);
+
+		//then
+		assertAll(
+				() -> assertThat(응답).hasSize(1),
+				() -> assertThat(응답.get(0).getThumb()).isEqualTo((25 + 26 + 27 + 30 + 35) / 5)
+		);
 	}
 }
+
