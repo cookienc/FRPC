a237522bd0072a946946ee42a7b6e18574db4bb2
kookiedncream <kim76744@naver.com>
2022-10-10 19:37:24 +0900

    feat: 홈 화면 수정

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,18 +1,5 @@
 <template>
-  <HelloWorld />
+  <div>
+    <h1>스물일곱 스물다섯</h1>
+  </div>
 </template>
-
-<script>
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
-});
-</script>
