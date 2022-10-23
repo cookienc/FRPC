bde27c891114cb99c80f44a801c9184d641980f5
kookiedncream <kim76744@naver.com>
2022-10-23 15:36:20 +0900

    feat: 기준값 제거

--- src/main/java/rehabilitation/device/model/dto/SensorBarGraphResponse.java
+++ src/main/java/rehabilitation/device/model/dto/SensorBarGraphResponse.java
@@ -3,18 +3,14 @@ package rehabilitation.device.model.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import static rehabilitation.device.model.dto.PressureCriteria.*;
-
 @Getter
 public class SensorBarGraphResponse {
 	private PressureResponse pressures;
-	private PressureCriteria criteria;
 	private FlexResponse flexes;
 
 	@Builder
-	public SensorBarGraphResponse(PressureResponse pressures, PressureCriteria criteria, FlexResponse flexes) {
+	public SensorBarGraphResponse(PressureResponse pressures, FlexResponse flexes) {
 		this.pressures = pressures;
-		this.criteria = criteria;
 		this.flexes = flexes;
 	}
 
@@ -22,7 +18,6 @@ public class SensorBarGraphResponse {
 		return SensorBarGraphResponse.builder()
 				.pressures(pressureResponse)
 				.flexes(flexResponse)
-				.criteria(getPressureCriteria())
 				.build();
 	}
 }
